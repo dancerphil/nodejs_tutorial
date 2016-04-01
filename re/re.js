b9df45c1fe19725390f933cec067e82271298e2f
dancerphil <413149044@qq.com>
2016-04-01 13:41:52 +0800

    re: 删去重复方法

--- re/re.js
+++ re/re.js
@@ -8,12 +8,7 @@ fs.readFile('data.txt','utf8', (err, data) => {
 });
 var foo = function (item, index, array) {
 	console.log(item);
-	var re3 = /^[^]*[\u4e00-\u9fa5]+[^]*$/
-	var re4 = /^[^]*[0-9]{7,}[^]*$/
-	if(re3.test(item)&!re4.test(item)) {
-		// console.log('\t'+item);
-	}
-	else{}
+
 	// 1、字符中存在汉字，2、字符中任何位置没有连续的7位数字
 	var reg = /^(?![^0-9]*([0-9]{7,}))[^]*[\u4e00-\u9fa5]+[^]*$/
 	if(reg.test(item)) {
