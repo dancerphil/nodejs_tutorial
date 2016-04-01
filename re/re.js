45225241a8c58eb3095b1488105a52c8a1006b99
dancerphil <413149044@qq.com>
2016-04-01 16:56:12 +0800

    re: 精简

--- re/re.js
+++ re/re.js
@@ -10,7 +10,7 @@ var foo = function (item, index, array) {
 	console.log(item);
 
 	// 1、字符中存在汉字，2、字符中任何位置没有连续的7位数字
-	var reg = /^(?![^0-9]*([0-9]{7,}))[^]*[\u4e00-\u9fa5]+[^]*$/
+	var reg = /(?![^0-9]*([0-9]{7,}))[\u4e00-\u9fa5]+/
 	if(reg.test(item)) {
 		console.log('\t\t'+item)
 		resolveStream.write(item+'\r\n','UTF8')
