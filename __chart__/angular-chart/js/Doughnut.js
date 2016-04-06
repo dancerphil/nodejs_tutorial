0c2b7dabebb4a2060abfeee8922b665db8d6793c
dancerphil <413149044@qq.com>
2016-04-06 17:09:10 +0800

    add doughnut

---
+++ __chart__/angular-chart/js/Doughnut.js
@@ -0,0 +1,5 @@
+angular.module("app", ["chart.js"])
+	.controller("DoughnutCtrl", function ($scope) {
+	$scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
+	$scope.data = [300, 500, 100];
+});
