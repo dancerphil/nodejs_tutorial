843913d0940f868731113c49536322967e704cad
dancerphil <413149044@qq.com>
2016-04-15 16:44:38 +0800

    re 改

--- re/re.js
+++ re/re.js
@@ -10,7 +10,18 @@ var foo = function (item, index, array) {
 	console.log(item);
 
 	// 1、字符中存在汉字，2、字符中任何位置没有连续的7位数字
-	var reg = /(?![^0-9]*([0-9]{7,}))[\u4e00-\u9fa5]+/
+	var reg1 = /[\u4e00-\u9fa5]+/
+	var regNum = /[0-9]{7,}/
+	if(reg1.test(item)&&!regNum.test(item)) {
+		console.log('\t\t'+item)
+	}
+	else{
+	}
+
+
+	// 1、字符中存在汉字，2、字符中任何位置没有连续的7位数字
+	// var reg = /(?!([0-9]{7,}))[\u4e00-\u9fa5]+/
+	var reg = /^(?!.*[0-9-_]{7,}).*([\u4E00-\u9FA3]+)/
 	if(reg.test(item)) {
 		console.log('\t\t'+item)
 		resolveStream.write(item+'\r\n','UTF8')
