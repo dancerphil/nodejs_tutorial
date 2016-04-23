d59e742875e251fec3f623ff57fee2364a1da853
dancerphil <413149044@qq.com>
2016-04-24 01:55:14 +0800

    combine step1 add step2

--- knockoutjs/step1.js
+++
@@ -1,8 +0,0 @@
-// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
-function AppViewModel() {
-    this.firstName = "Bert";
-    this.lastName = "Bertington";
-}
-
-// Activates knockout.js
-ko.applyBindings(new AppViewModel());
