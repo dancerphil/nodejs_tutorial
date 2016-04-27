69ea28f59ad67f070b29e3c6cdcd7c30fad714de
dancerphil <413149044@qq.com>
2016-04-27 14:29:05 +0800

    Asynchronous

---
+++ socketio/asynchronous.js
@@ -0,0 +1,22 @@
+var io = require('socket.io')()
+var Datastore = require('nedb')
+  , db = new Datastore({ filename: 'data/tokens.db' });
+db.loadDatabase();
+
+io.listen(80)
+io.sockets.use(function(socket, next) {
+	console.log("in use")
+	db.count({}, function (err, count) {
+		console.log('db count: '+count)
+		next()
+	});
+})
+.on('connection', function (socket) {
+	console.log("in con")
+	socket.on('fff', function (data) {
+		console.log("in fff")
+		db.insert(data)
+		console.log(data)
+	})
+})
+console.log('The bus socket service listen on port ' + 80);
