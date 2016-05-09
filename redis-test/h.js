a9d8394075572cdf35489930bf56b1ccc4b100d7
dancerphil <413149044@qq.com>
2016-05-09 14:36:47 +0800

    hgetall

--- redis-test/h.js
+++ redis-test/h.js
@@ -1,20 +1,34 @@
 var redis = require("redis")
   , client = redis.createClient();
 
-client.keys('*',function(err, ks){
-	console.log(ks)
-	ks.forEach(function (k, i) {
-		console.log(k)
-		client.hkeys('socket',function(err, replies){
-			replies.forEach(function (reply, i) {
-				client.hdel('socket', reply, redis.print)
-			});
-		})
-	})
+// client.keys('*',function(err, ks){
+// 	console.log(ks)
+// 	ks.forEach(function (k, i) {
+// 		console.log(k)
+// 		client.hkeys('socket',function(err, replies){
+// 			replies.forEach(function (reply, i) {
+// 				client.hdel('socket', reply, redis.print)
+// 			});
+// 		})
+// 	})
 
+// })
+
+// client.hset("test", 'keys maybe token2', 'JSON.stringify(item)2', redis.print);
+
+// client.hkeys("test", function (err, replies) {
+// 		if(err){
+// 			console.log(err)
+// 		} else {
+// 			console.log(replies.length + " replies:");
+// 			replies.forEach(function (reply, i) {
+// 				client.hget("test", reply, function(a,b){
+// 					console.log(a,b)
+// 				})
+// 			});
+// 		}
+// 	});
+
+client.hgetall("test", function (err, replies) {
+	console.log(replies)
 })
-
-
-
-
-
